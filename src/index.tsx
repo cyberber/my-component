470dbaa4f9603cddd29c5119d580e9066b8922bc
david.chen <978265004@qq.com>
2024-07-24 19:50:45 +0800

    feat: 初始化提交

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './styles/index.scss'
 const a = 2;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div>{ a == 2 ? 1 : '2'}</div>
+    <h1>HELLO WORLD</h1>
+    <h2>HELLO WORLD</h2>
+    <h3>HELLO WORLD</h3>
+    <h4>HELLO WORLD</h4>
+    <h5>HELLO WORLD</h5>
   </React.StrictMode>,
 )
